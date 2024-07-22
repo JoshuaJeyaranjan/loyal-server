5d1ffc33b886019f0b49a0ce0d8b3d44924ada82
Joshua Jeyaranjan <joshuajeyaranjan@macbook-pro-5.lan>
2024-07-22 16:29:31 -0400

    display product details

---
+++ migrations/20240722172955_add_notifications_to_users 2.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.table('users', function(table) {
+      table.boolean('email_notifications'); // Add category column
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema.table('ysers', function(table) {
+      table.dropColumn('email_notifications'); // Remove category column if rolling back
+    });
+  };
+  
